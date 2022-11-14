33d32d7c73954740f4a7829eb04244c2591b9e9f
andresgur <38692538+andresgur@users.noreply.github.com>
2022-11-14 12:01:43 +0000

    Fixed bug in coordinate parsing

--- coordinates.py
+++ coordinates.py
@@ -5,9 +5,11 @@ def convert_ra_dec(ra, dec):
     """Convert strings of Right Ascension and Declination into decimal format"""
     if ":" in ra:
         split_ra = ra.split(":")
+        ra = (float(split_ra[0]) + float(split_ra[1]) / 60 + float(split_ra[2]) / 3600) / 24 * 360
     elif " " in ra:
         split_ra = ra.split(" ")
         ra = (float(split_ra[0]) + float(split_ra[1]) / 60 + float(split_ra[2]) / 3600) / 24 * 360
+
     else:
         ra = float(ra)
 
@@ -17,10 +19,13 @@ def convert_ra_dec(ra, dec):
     elif " " in dec:
         split_dec = dec.split(" ")
         dec = float(split_dec[0])
-        if dec<0:
-            dec = float(split_dec[0]) - float(split_dec[1]) / 60 - float(split_dec[2]) / 3600
-        else:
-            dec =  float(split_dec[0]) + float(split_dec[1]) / 60  + float(split_dec[2]) / 3600
     else:
         dec = float(dec)
+        return ra, dec
+
+    if dec<0:
+        dec = float(split_dec[0]) - float(split_dec[1]) / 60 - float(split_dec[2]) / 3600
+    else:
+        dec = float(split_dec[0]) + float(split_dec[1]) / 60  + float(split_dec[2]) / 3600
+
     return ra, dec
