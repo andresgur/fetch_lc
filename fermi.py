b37fceb4ee7cf1aba67c0169156823c0a3dce48a
andresgur <38692538+andresgur@users.noreply.github.com>
2023-08-18 20:18:56 +0100

    First version for fermi LAT

--- fermi.py
+++ fermi.py
@@ -1,18 +1,14 @@
 import matplotlib.pyplot as plt
 import argparse
 import os
-import time
-import sys
-import lightkurve as lk
 import numpy as np
-from uncertainties.umath import log10 as ulog
-from uncertainties import ufloat
-from uncertainties import unumpy
-import math as m
-from coordinates import convert_ra_dec
 from astroquery.simbad import Simbad
 import warnings
 import pyLCR
+from fetch_lc import create_outdir, get_source_names
+from coordinates import convert_ra_dec
+
+delimiter = ","
 
 ap = argparse.ArgumentParser(description='Fetch data from the Fermi-LAT lightcurve repository (https://fermi.gsfc.nasa.gov/ssc/data/access/lat/LightCurveRepository/index.html)')
 ap.add_argument("--ra", help="Right ascension", type=str, nargs="?", required=True)
@@ -20,35 +16,24 @@ ap.add_argument("--dec", help="Declination", type=str, nargs="?", required=True)
 ap.add_argument("-o", "--outdir", nargs='?', help="Output dir name", type=str, default="data")
 args = ap.parse_args()
 
-zero_point = ufloat(20.44, 0.05)
-
-ra = args.ra
-dec = args.dec
+ra_str = args.ra
+dec_str = args.dec
 outdir = args.outdir
 
-ra, dec = convert_ra_dec(ra, dec)
-
-ra_dec = "%.12f %+.12f" % (ra, dec)
-print("(RA, Dec) = (%s)" % (ra_dec))
-
 if not os.path.isdir(outdir):
     os.mkdir(outdir)
-## KEPLER data
 
-result_table = Simbad.query_region("%.5f, %+.5f" % (ra, dec))
-names = result_table["MAIN_ID"]
-name = names[0]
-print(" Source name\n ------- \n %s" % name)
-outdir += "/%s" % name
+ra, dec = convert_ra_dec(ra_str, dec_str)
+name = get_source_names(ra, dec)[0]
 
-if not os.path.isdir(outdir):
-    os.mkdir(outdir)
+outdir = create_outdir(outdir, name)
 # https://indico.cern.ch/event/66737/contributions/2063638/attachments/1018688/1449988/MAPS_razzano.pdf slide 22
 MJDREFI = 51910
 MJDREFF = 7.42870370370370 * 10**-4
 # get all identifiers, we need the 4FGL
-result_table = Simbad.query_objectids(names[0])
+result_table = Simbad.query_objectids(name)
 # Assuming you have the result_table as shown in your question
+fgl_idenitfier = None
 for row in result_table["ID"]:
     if '4FGL' in row:
         fgl_idenitfier = row
@@ -70,16 +55,17 @@ else:
     upp_lims = data.ts < data.ts_min
     outputs = np.array([data.met_detections / 3600 / 24 + MJDREFF + MJDREFI, data.flux, data.flux_error.T[0], data.flux_error.T[1], data.ts[~upp_lims],
                         data.photon_index,
-                        data.photon_index_interval, data.fit_convergence[~upp_lims]])
-    header = "#MJD\tflux\tflux_err_neg\tflux_err_pos\tts\tphoton_index\tphoton_index_err\tfit_convergence"
+                        data.photon_index - data.photon_index_interval, data.fit_convergence[~upp_lims]])
+    header = "MJD,flux,flux_err_neg,flux_err_pos,ts,photon_index,photon_index_err,fit_convergence"
 
-    outputfile = "fermi_lat.dat"
+    outputfile = "%s/fermi_lat.dat" % outdir
+
     np.savetxt(outputfile, outputs.T,
-               delimiter="\t", fmt="%.6f", header=header)
+               delimiter=delimiter, fmt="%.6e", header=header)
     # save upper limits
 
     outputs = np.array([data.met_upperlimits  / 3600 / 24 + MJDREFF + MJDREFI, data.flux_upper_limits, data.ts[upp_lims], data.fit_convergence[upp_lims]])
-    outputfile = "fermi_lat_upplims.dat"
-    header = "#MJD\tflux\tts\tphoton_index\tphoton_index_err\tfit_convergence"
+    outputfile = "%s/fermi_lat_upplims.dat" % outdir
+    header = "MJD,flux,ts,photon_index,photon_index_err,fit_convergence"
     np.savetxt(outputfile, outputs.T,
-               delimiter="\t", fmt="%.6f", header=header)
+               delimiter=delimiter, fmt="%.6e", header=header)
