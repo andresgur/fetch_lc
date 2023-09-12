88f03296127b28eb85d52b987327b05baef05663
andresgur <38692538+andresgur@users.noreply.github.com>
2023-09-12 19:37:41 +0100

    Stores exposure times now

--- ztf.py
+++ ztf.py
@@ -6,10 +6,9 @@ import sys
 from ztfquery import lightcurve
 import numpy as np
 from uncertainties.umath import log10 as ulog
-from uncertainties import ufloat
 from uncertainties import unumpy
 from coordinates import convert_ra_dec
-from astroquery.simbad import Simbad
+from fetch_lc import create_outdir, get_source_names
 
 # https://www.ptf.caltech.edu/page/lcgui
 ap = argparse.ArgumentParser(description='Fetch data from ZTF https://irsa.ipac.caltech.edu/docs/program_interface/ztf_lightcurve_api.html')
@@ -17,8 +16,6 @@ ap.add_argument("--ra", help="Right ascension", type=str, nargs="?", required=Tr
 ap.add_argument("--dec", help="Declination", type=str, nargs="?", required=True)
 ap.add_argument("-o", "--outdir", nargs='?', help="Output dir name", type=str, default="data")
 args = ap.parse_args()
-
-zero_point = ufloat(20.44, 0.05)
 
 ra = args.ra
 dec = args.dec
@@ -32,13 +29,8 @@ print("(RA, Dec) = (%s)" % (ra_dec))
 if not os.path.isdir(outdir):
     os.mkdir(outdir)
 
-result_table = Simbad.query_region("%.5f, %+.5f" % (ra, dec))
-name = result_table[0]["MAIN_ID"]
-print("Source name\n ------ \n%s" % name)
-outdir += "/%s" % name
-
-if not os.path.isdir(outdir):
-    os.mkdir(outdir)
+name = get_source_names(ra, dec)[0]
+outdir = create_outdir(outdir, name)
 
 lcq = lightcurve.LCQuery.from_position(ra, dec, 5) # radius in arcsec
 
@@ -52,14 +44,16 @@ else:
 
     bands = ["zg", "zr", "zi"]
     for band in bands:
-        band_filter = np.where(data["filtercode"].values==band)
+        band_filter = data["filtercode"].values==band
         times = data['mjd'].values[band_filter]
         mags = data['mag'].values[band_filter]
         mag_err = data['magerr'].values[band_filter]
+        exptime = data["exptime"].values[band_filter]
+        catflags = data["catflags"].values[band_filter]
         plt.errorbar(times, mags, yerr=mag_err,
                      ls='None', label="%s" % band) # , fmt="+"
         outputfile = "%s/ztf_%s.dat" %(outdir, band)
-        np.savetxt(outputfile, np.asarray([times, mags, mag_err]).T,
-                   delimiter="\t", fmt="%.6f", header="t\tmag\terr")
+        np.savetxt(outputfile, np.asarray([times, mags, mag_err,exptime, catflags]).T,
+                   delimiter="\t", fmt="%.6f", header="t\tmag\terr\texposures\tcatflags")
     ax.legend()
     plt.savefig("%s/ztf.png" % outdir)
